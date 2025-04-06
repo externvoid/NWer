d03fc00122dc02f4395e78109e000eac9c6a6d18
externvoid <hrtanaka@gmail.com>
2025-04-06 19:53:43 +0900

    feat: update the crawling.db using split/merger rate feat: download the 'recent.csv' containing the date header

--- Sources/NWer/ext_Foo.swift
+++ Sources/NWer/ext_Foo.swift
@@ -1,4 +1,5 @@
 //  Created by Tanaka Hiroshi on 2025/02/11.
+//  modified by Tanaka Hiroshi on 2025/04/05.
 import Foundation
 import SQLite
 // エラーハンドリング用の型定義
@@ -10,29 +11,37 @@ enum DownloadError: Error {
 
 // CSVデータの構造体定義
 public struct CSVData {
-  let code: String
-  let open: Double
-  let high: Double
-  let low: Double
-  let close: Double
-  let volume: Double
-  let adj: Double
+  let date: String
+  var ar: [OHLCVA]
+  init(_ date: String, _ ar: [OHLCVA]) {
+    self.date = date
+    self.ar = ar
+  }
+  struct OHLCVA {
+    let code: String
+    let open: Double
+    let high: Double
+    let low: Double
+    let close: Double
+    let volume: Double
+    let adj: Double
 
-  init(_ code: String,_  open: Double,_  high: Double,
-       _ low: Double,_  close: Double,_  volume: Double,_  adj: Double) {
-    self.code = code
-    self.open = open
-    self.high = high
-    self.low = low
-    self.close = close
-    self.volume = volume
-    self.adj = adj
+    init(_ code: String,_  open: Double,_  high: Double,
+         _ low: Double,_  close: Double,_  volume: Double,_  adj: Double) {
+      self.code = code
+      self.open = open
+      self.high = high
+      self.low = low
+      self.close = close
+      self.volume = volume
+      self.adj = adj
+    }
   }
 }
 
-let ar: [CSVData] = [CSVData("0000",260.0,261.0,255.0,256.0,313600.0,256.0),
-   CSVData("397A",260.0,261.0,255.0,256.0,313600.0,256.0),
-   CSVData("300A",986.0,1008.0,971.0,981.0,51300.0,981.0)]
+let ar: [CSVData.OHLCVA] = [CSVData.OHLCVA("0000",260.0,261.0,255.0,256.0,313600.0,256.0),
+   CSVData.OHLCVA("397A",260.0,261.0,255.0,256.0,313600.0,256.0),
+   CSVData.OHLCVA("300A",986.0,1008.0,971.0,981.0,51300.0,981.0)]
 
 extension Networker {
   // MARK: UpdateFromWebAPI
@@ -40,10 +49,10 @@ extension Networker {
   @available(macOS 12.0, *) // upDateFromWebAPI
   public static func updateFromWebAPI(_ dbPath1: String, _ dbPath2: String) ->  Void {
 
-    let ar = try! downloadAndParseCSV()
-    let recent: String = try! getDate()
+    let csv: CSVData = try! downloadAndParseCSV()
+//    let recent: String = try! getDate)
 
-    for (i, e) in ar.enumerated() {
+    for (i, e) in csv.ar.enumerated() {
       if i % 500 == 0 {
         print("i: \(i), e: \(e.code)")
       }
@@ -56,44 +65,102 @@ extension Networker {
 //        print("1301 found")
 //        break
 //      }
-      let tbl: Table = Table(e.code)
-//      let date = Expression<String>("date")
-
-      do {
-        // データベースに接続
-        let db = try Connection(dbPath)
-
-        // usersテーブルの存在を確認するクエリ
-        let query = "SELECT name FROM sqlite_master WHERE type='table' AND name='\(e.code)';"
-        let insert = tbl.insert(date <- recent, open <- e.open, high <- e.high, low <- e.low, close <- e.close, volume <- e.volume, adj <- e.adj)
-
-        // クエリを実行
-        if let _ = try? db.prepare(query).next() {
-          let _ = try db.run(insert)
-//          print("\(e.code): テーブルが存在します。")
-        } else {
-          try db.run(tbl.create { t in
-            t.column(date, primaryKey: true)
-            t.column(open)
-            t.column(high)
-            t.column(low)
-            t.column(close)
-            t.column(volume)
-            t.column(adj)
-          })
-          let _ = try db.run(insert)
-//          print("\(e.code): テーブルは存在しません。")
-        }
-
-      } catch {
-        print("エラーが発生しました: \(error)")
+      if e.adj < 0.0 {
+        adjUpdate(e, csv.date, dbPath)
+      } else {
+        planeUpdate(e, csv.date, dbPath)
       }
+    }
+  }
+  static func adjUpdate(_ e: CSVData.OHLCVA, _ dateStr: String, _ dbPath: String) {
+    let tbl: Table = Table(e.code)
+    let rate: Double = abs(e.adj)
+    do {
+      // データベースに接続
+      let db = try Connection(dbPath)
+      let update = tbl.update(Networker.adj <- Networker.close / rate)
+      // クエリを実行
+      let _ = try db.run(update)
+
+    } catch {
+      print("エラーが発生しました@adjUpdate: \(error)")
+    }
+    planeUpdate2(e, dateStr, dbPath)
+  }
+  static func planeUpdate2(_ e: CSVData.OHLCVA, _ dateStr: String, _ dbPath: String) {
+    let tbl: Table = Table(e.code)
+    do {
+      // データベースに接続
+      let db = try Connection(dbPath)
+
+      // usersテーブルの存在を確認するクエリ
+      let query = "SELECT name FROM sqlite_master WHERE type='table' AND name='\(e.code)';"
+      let insert = tbl.insert(Networker.date <- dateStr,
+          Networker.open <- e.open, Networker.high <- e.high, Networker.low <- e.low,
+          Networker.close <- e.close, Networker.volume <- e.volume,
+          Networker.adj <- e.close) // note: e.adj is rate
+      // e.adjの値に応じて処理を分岐
+      // クエリを実行
+      if let _ = try? db.prepare(query).next() {
+        let _ = try db.run(insert)
+        //          print("\(e.code): テーブルが存在します。")
+      } else {
+        try db.run(tbl.create { t in
+          t.column(Networker.date, primaryKey: true)
+          t.column(Networker.open)
+          t.column(Networker.high)
+          t.column(Networker.low)
+          t.column(Networker.close)
+          t.column(Networker.volume)
+          t.column(Networker.adj)
+        })
+        let _ = try db.run(insert)
+        //          print("\(e.code): テーブルは存在しません。")
+      }
+
+    } catch {
+      print("エラーが発生しました@planeUpdate: \(error)")
+    }
+  }
+  static func planeUpdate(_ e: CSVData.OHLCVA, _ dateStr: String, _ dbPath: String) {
+    let tbl: Table = Table(e.code)
+    do {
+      // データベースに接続
+      let db = try Connection(dbPath)
+
+      // usersテーブルの存在を確認するクエリ
+      let query = "SELECT name FROM sqlite_master WHERE type='table' AND name='\(e.code)';"
+      let insert = tbl.insert(Networker.date <- dateStr, Networker.open <- e.open, Networker.high <- e.high, Networker.low <- e.low, Networker.close <- e.close, Networker.volume <- e.volume, Networker.adj <- e.adj)
+      // e.adjの値に応じて処理を分岐
+      // クエリを実行
+      if let _ = try? db.prepare(query).next() {
+        let _ = try db.run(insert)
+        //          print("\(e.code): テーブルが存在します。")
+      } else {
+        try db.run(tbl.create { t in
+          t.column(Networker.date, primaryKey: true)
+          t.column(Networker.open)
+          t.column(Networker.high)
+          t.column(Networker.low)
+          t.column(Networker.close)
+          t.column(Networker.volume)
+          t.column(Networker.adj)
+        })
+        let _ = try db.run(insert)
+        //          print("\(e.code): テーブルは存在しません。")
+      }
+
+    } catch {
+      print("エラーが発生しました@planeUpdate: \(error)")
     }
   }
 
   // CSVファイルをダウンロードしてパースする関数
   //let url: String = "https://tw.lan/recent/recent.csv"
-  public static func downloadAndParseCSV(from url: String = "https://stock.bad.mn/recent/recent.csv") throws -> [CSVData] {
+
+  // MARK: DownloadAndParseCSV
+
+  public static func downloadAndParseCSV(from url: String = "https://stock.bad.mn/recent/recent.csv") throws -> CSVData {
     guard let url = URL(string: url) else {
       throw DownloadError.invalidUrl
     }
@@ -106,10 +173,12 @@ extension Networker {
     // 行に分割（1行目はヘッダーなのでスキップ）
     let lines = content.components(separatedBy: "\n") //.dropFirst()
     let filtered = lines.filter{ !$0.isEmpty } // 最後の空行を削除
-    var result: [CSVData] = []
+    let dateStr: String! = filtered.first; let ohlcs = filtered.dropFirst()
+    var result: CSVData = CSVData(dateStr, [])
+    print("date: \(result.date)")
 
     // 各行を処理
-    for line in filtered {
+    for line in ohlcs {
       // カンマで区切って要素を取得
       let components = line.components(separatedBy: ",")
 
@@ -128,7 +197,7 @@ extension Networker {
         throw DownloadError.invalidFormat
       }
 
-      result.append(CSVData(components[0], open, high, low, close, volume, adj))
+      result.ar.append(CSVData.OHLCVA(components[0], open, high, low, close, volume, adj))
     }
 
     return result
